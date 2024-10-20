e79e38505e198be311325ca5db9029a8e791b51f
unknown <rahulpawar2001.rp@gmail.com>
2024-10-20 23:07:22 +0530

    first and second steps are done

---
+++ week6/week-6-web3-token-launchpad/1-token-launchpad-starter/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { TokenLaunchpad } from './components/TokenLaunchpad'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+function App() {
+  return (
+    <div>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+            <WalletModalProvider>
+              <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                padding: 20
+              }}>
+                <WalletMultiButton />
+              </div>
+              <TokenLaunchpad></TokenLaunchpad>
+            </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
+  )
+}
+
+
+export default App
---
+++ week6/week-6-web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -0,0 +1,25 @@
+
+export function TokenLaunchpad() {
+
+    function createToken() {
+        const name = document.getElementById('name').value;
+        const symbol = document.getElementById('symbol').value;
+        const image = document.getElementById('image').value;
+        const initialSupply = document.getElementById('initialSupply').value;
+    }
+
+    return  <div style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column'
+    }}>
+        <h1>Solana Token Launchpad</h1>
+        <input className='inputText' type='text' placeholder='Name'></input> <br />
+        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
+        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
+        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
+        <button onClick={createToken} className='btn'>Create a token</button>
+    </div>
+}
