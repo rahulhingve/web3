e4d0c522f60e79859f5a28b1fc38b5f1196dd2b0
unknown <rahulpawar2001.rp@gmail.com>
2024-10-23 16:39:36 +0530

    mint function correction

--- week6/week-6-web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ week6/week-6-web3-token-launchpad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,12 +1,32 @@
-import { createMint } from "@solana/spl-token";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { MINT_SIZE, TOKEN_PROGRAM_ID, createInitializeMint2Instruction, createMint, getMinimumBalanceForRentExemptMint } from "@solana/spl-token"
 
 export function TokenLaunchpad() {
+    const { connection } = useConnection();
+    const wallet = useWallet();
 
-    function createToken() {
-        const name = document.getElementById('name').value;
-        const symbol = document.getElementById('symbol').value;
-        const image = document.getElementById('image').value;
-        const initialSupply = document.getElementById('initialSupply').value;
+    async function createToken() {
+        const mintKeypair = Keypair.generate();
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID,
+            }),
+            createInitializeMint2Instruction(mintKeypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID)
+        );
+            
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+        transaction.partialSign(mintKeypair);
+
+        await wallet.sendTransaction(transaction, connection);
+        console.log(`Token mint created at ${mintKeypair.publicKey.toBase58()}`);
     }
 
     return  <div style={{
@@ -24,13 +44,3 @@ export function TokenLaunchpad() {
         <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
-
-
-const mint = await createMint(
-        connection,
-        payer,
-        mintAuthority,
-        null,
-        6,
-        TOKEN_PROGRAM_ID
-);
